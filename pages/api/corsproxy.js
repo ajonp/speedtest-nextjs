490eb4eb670bf6d62e1e651391f35175621b09a5
Alex Patterson <alex@codingcat.dev>
2022-01-31 12:56:16 -0500

    show in function log hostname

--- pages/api/corsproxy.js
+++ pages/api/corsproxy.js
@@ -9,6 +9,7 @@ const allowCors = fn => async (req, res) => {
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   )
   if (req.method === 'OPTIONS') {
+    console.log('Added CORS to: ', req.hostname)
     res.status(200).end()
     return
   }
