4040d777d3b56e237233f5a806becdbbaaa4f660
Alex Patterson <alex@codingcat.dev>
2022-01-31 11:36:54 -0500

    before partytown on gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import { useEffect } from 'react'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import { GTM_ID, pageview } from '../lib/gtm'
+import { Partytown } from "@builder.io/partytown/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  useEffect(() => {
+    router.events.on('routeChangeComplete', pageview)
+    return () => {
+      router.events.off('routeChangeComplete', pageview)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      {/* Google Tag Manager - Global base code */}
+      <Script
+        id="googl-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
+      <Partytown debug={true} />
+      <script type='text/partytown' dangerouslySetInnerHTML={{ __html: `console.log('adam')` }} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
