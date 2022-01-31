0ace4b4c63ca60b48419f31ecb001c0a125d8fbb
Alex Patterson <alex@codingcat.dev>
2022-01-31 13:12:54 -0500

    use nextjs script tag

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import Script from 'next/script'
 import { useRouter } from 'next/router'
 import { GTM_ID, pageview } from '../lib/gtm'
-import { Partytown, GoogleTagManagerNoScript } from "@builder.io/partytown/react";
+import { Partytown } from "@builder.io/partytown/react";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
@@ -15,7 +15,21 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <GoogleTagManagerNoScript containerId={GTM_ID} />
+      {/* Google Tag Manager - Global base code */}
+      <Script
+        id="googl-tag-manager"
+        strategy="afterInteractive"
+        type='text/partytown'
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
       <Partytown
         debug={true}
         resolveUrl={(url) => {
